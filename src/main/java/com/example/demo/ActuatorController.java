13601eb2ccc0e2a282e0c25d574eb4be66a4b9a1
mivanova1 <mivanova@pivotal.io>
2017-06-23 09:52:14 +0100

    Second Commit

--- src/main/java/com/example/demo/ActuatorController.java
+++ src/main/java/com/example/demo/ActuatorController.java
@@ -1,4 +1,4 @@
-/* package com.example.demo;
+ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +26,3 @@ public class ActuatorController {
 
  
 }
-*/
